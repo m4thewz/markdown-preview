4aa849b74ed95af1806f51d4534dde37fe2c5c86
Matheus Vitor <m4theusdev@gmail.com>
2024-02-01 13:14:56 -0300

    feat: create markdown preview section

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# markdown-preview
- Real-time markdown preview
+# Markdown Preview
+
+This is a project that allows real-time viewing of Markdown. You can type your Markdown text and instantly see how it will be displayed, making editing and formatting easier.
+
+## Features
+
+- **Real-Time Update:** Visualize changes instantly as you type.
+- **Markdown Editor:** Use the left editor to format your text with Markdown.
+- **User-Friendly:** Simple and intuitive interface for a hassle-free user experience.
+
+If you want to contribute to this project, pull request are welcome.
+
+## License
+
+This project is licensed under the MIT License.
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,64 @@
-const editor = ace.edit("edit", {
+const checkbox = document.querySelector("#sync-scroll");
+let scrollBarSync = false;
+
+function convertMarkdown(code) {
+  const html = marked.parse(code, { headerIds: false, mangle: false });
+  document.querySelector("#preview").innerHTML = DOMPurify.sanitize(html);
+}
+
+function saveContent(code) {
+  localStorage.setItem("editorValue", code);
+}
+
+function copyContent() {
+  navigator.clipboard.writeText(localStorage.getItem("editorValue"));
+  alert("Text copied to clipboard");
+}
+
+const editor = ace.edit("editor", {
   wrap: true,
+  maxLines: Infinity,
+  indentedSoftWrap: false,
+  fontSize: 12,
+  autoScrollEditorIntoView: true,
   theme: "ace/theme/textmate",
   mode: "ace/mode/markdown",
   keyboardHandler: "ace/keyboard/vscode",
 });
+
+editor.on("change", () => {
+  const code = editor.getValue();
+  convertMarkdown(code);
+  saveContent(code);
+});
+
+const editorValue = localStorage.getItem("editorValue");
+if (!editorValue) {
+  fetch("../README.md")
+    .then((response) => response.text())
+    .then((data) => {
+      localStorage.setItem("editorValue", data);
+      editor.session.insert(editor.getCursorPosition(), data);
+    });
+} else {
+  editor.session.insert(editor.getCursorPosition(), editorValue);
+}
+
+checkbox.addEventListener("change", (e) => {
+  scrollBarSync = e.currentTarget.checked;
+});
+
+document
+  .querySelector("#edit .ace_scrollbar")
+  .addEventListener("scroll", (e) => {
+    if (!scrollBarSync) return;
+
+    const { scrollTop, scrollHeight, clientHeight } = e.currentTarget;
+    const ratio = scrollTop / (scrollHeight - clientHeight);
+
+    const previewElement = document.querySelector("#preview");
+    const targetY =
+      (previewElement.scrollHeight - previewElement.clientHeight) * ratio;
+
+    previewElement.scrollTo(0, targetY);
+  });
